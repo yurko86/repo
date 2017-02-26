093035b5b26df8ad14e2e6d877ed684772c675ad
matrix\iurii <magnyfyer@gmail.com>
2017-02-26 23:20:41 +0100

    no message

--- HelloUserFromFile/src/hellouserfromfile/HelloUserFromFile.java
+++ HelloUserFromFile/src/hellouserfromfile/HelloUserFromFile.java
@@ -4,10 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 /**
